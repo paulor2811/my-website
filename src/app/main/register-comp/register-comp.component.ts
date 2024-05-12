b8d3b0054e3854b5591abdfd9d617e1809b872f8
Paulo <paulo.ricardo.aleixo3@gmail.com>
2024-05-12 14:39:33 -0300

    entered registration confirmation

--- src/app/main/register-comp/register-comp.component.ts
+++ src/app/main/register-comp/register-comp.component.ts
@@ -1,20 +1,23 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { HttpErrorResponse } from '@angular/common/http';
-import { UserService } from '../../utils/user.service'
+import { UserService } from '../../utils/user.service';
 
 @Component({
   selector: 'app-register-comp',
   templateUrl: './register-comp.component.html',
-  styleUrl: './register-comp.component.css'
+  styleUrls: ['./register-comp.component.css']
 })
 export class RegisterCompComponent {
-  
+  registroBemSucedido: boolean = false;
+  registroFalhou: boolean = false; // Variável para controlar a exibição da mensagem de erro
+
   constructor(private router: Router, private userService: UserService) {}
   
   irParaHome() {
     this.router.navigate(['/home']);
   }
+  
   registrarUsuario() {
     const nome = (document.getElementById('InputName1') as HTMLInputElement).value;
     const email = (document.getElementById('InputEmail1') as HTMLInputElement).value;
@@ -23,13 +26,17 @@ export class RegisterCompComponent {
     const usuario = { nome, email, senha };
 
     this.userService.registrarUsuario(usuario)
-    .subscribe(
-    (response: any) => {
-      console.log('Usuário registrado com sucesso!', response);
-      // Redirecionar para a página inicial ou outra página desejada
-    },
-    (error: HttpErrorResponse) => {
-        console.error('Erro ao registrar usuário:', error);
-    });
+      .subscribe(
+        (response: any) => {
+          console.log('Usuário registrado com sucesso!', response);
+          this.registroBemSucedido = true; // Define a variável para true após o registro bem-sucedido
+          this.registroFalhou = false; // Reseta a variável de erro
+        },
+        (error: HttpErrorResponse) => {
+          console.error('Erro ao registrar usuário:', error);
+          this.registroFalhou = true; // Define a variável de erro para true
+          this.registroBemSucedido = false; // Reseta a variável de sucesso
+        }
+      );
   }
 }
